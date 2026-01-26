9a301bf1a8bae79437d1cdbafe1765d8430c142d
farhodjon8284-ctrl <farhodjon8284@gmail.com>
2026-01-26 15:09:59 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from pyrogram.enums import ChatMemberStatus
 # ========== CONFIG ==========
 API_ID = 743554
 API_HASH = "b440caaf2c2763fdc72bbb8350dfd6b8"
-BOT_TOKEN = "6610724048:AAG0169Sgenx8HTLWtW13cO-SZnTMwGkBL0"
+BOT_TOKEN = "6610724048:AAG2MxcmUeLZOhRvvXqhwv42p6eui4pShgw"
 
 ADMINS = [1940757150]
 CHANNEL_ID = "@Seriallar_kanaliuz"
@@ -16,7 +16,7 @@ app = Client(
     "Uzmovi_tarjima_bot",
     api_id=743554,
     api_hash="b440caaf2c2763fdc72bbb8350dfd6b8",
-    bot_token="6610724048:AAG0169Sgenx8HTLWtW13cO-SZnTMwGkBL0"
+    bot_token="6610724048:AAG2MxcmUeLZOhRvvXqhwv42p6eui4pShgw"
 )
 
 # ========== DATABASE ==========
