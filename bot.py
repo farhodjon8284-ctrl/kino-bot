da7d35856fc506d74cf15f13c8f4e4cb5215f2d2
farhodjon8284-ctrl <farhodjon8284@gmail.com>
2026-01-27 23:29:46 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 # ========= CONFIG =========
-API_ID = id:743554
+API_ID = id:743554,
 API_HASH = "b440caaf2c2763fdc72bbb8350dfd6b8"
 BOT_TOKEN = "6610724048:AAG2MxcmUeLZOhRvvXqhwv42p6eui4pShgw"
 
