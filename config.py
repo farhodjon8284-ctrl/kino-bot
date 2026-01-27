e902cf8316a9a2cb6a3c50ae28ac6685684fa87e
farhodjon8284-ctrl <farhodjon8284@gmail.com>
2026-01-28 00:36:02 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # ========= CONFIG =========
 API_ID = 743554
 API_HASH = "b440caaf2c2763fdc72bbb8350dfd6b8"
-BOT_TOKEN = "6610724048:AAG2MxcmUeLZOhRvvXqhwv42p6eui4pShgw"
+BOT_TOKEN = "8321768755:AAGVGzUi3T3nvrbFsR0RztA8bDOagqmyzO0"
 
 CHANNEL_ID = "@Seriallar_kanaliuz"   # majburiy obuna link
 ADMINS = [1940757150]
