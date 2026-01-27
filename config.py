538405adb1c868f029ea6a1c153ca07f309ae806
farhodjon8284-ctrl <farhodjon8284@gmail.com>
2026-01-28 00:25:55 +0500

    Create config.py with API and bot credentials
    Add configuration settings for API and bot.

---
+++ config.py
@@ -0,0 +1,8 @@
+# ========= CONFIG =========
+API_ID = 743554
+API_HASH = "b440caaf2c2763fdc72bbb8350dfd6b8"
+BOT_TOKEN = "6610724048:AAG2MxcmUeLZOhRvvXqhwv42p6eui4pShgw"
+
+CHANNEL_ID = "@Seriallar_kanaliuz"   # majburiy obuna link
+ADMINS = [1940757150]
+# ==========================
